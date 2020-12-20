49654acd0c3d68ecedfa2a262953a91dcf6f9e33
Glitch (hello-express) <none>
2020-12-20 14:23:00 +0000

    💓🐵 Checkpoint ./komutlar/mute.js:22659587/2210 ./komutlar/striga-ban.js:22659587/808

--- komutlar/mute.js
+++ komutlar/mute.js
@@ -1,7 +1,32 @@
-const { MessageEmed } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 const data = require('quick.db');
 const ms = require('ms');
 
 module.exports.run = async (client, message, args) => {
+if(!["", ""].some(role => message.member.roles.cache.get(role)) && (!message.member.hasPermission("ADMINISTRATOR"))) 
+return message.channel.send(new MessageEmbed().setDescription(`${message.author} Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
   
-}
+let member = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
+if (!member) return message.channel.send(new MessageEmbed().setColor('0x800d0d').setDescription(`${message.author}, lütfen bir kullanıcı etiketle !`))
+  
+  
+let mute = message.mentions.members.first() || message.guild.members.cache.find(c => c.id === args[0]);
+if (!mute) { new MessageEmbed().setColor('0x800d0d').setDescription(`${message.author}, lütfen mute atmam gereken kullanıcı belirt !`);
+} else {
+   if (mute.roles.highest.position >= message.member.roles.highest.position) 
+      {
+        return message.channel.send(new MessageEmbed().setColor('0x800d0d').setDescription(`Bu Kullanıcı Senden Üst/Aynı Pozisyonda.`));
+      } else {
+     let zaman = args[1]   
+     .replace("sn", "s")
+     .replace("dk", "m")
+     .replace("sa", "h")
+     .replace("gün", "d");
+      if (!zaman) { message.channel.send(new MessageEmbed().setColor('0x800d0d').setDescription(`Lütfen Bir zaman dilimi belirtin.`));
+      } else {
+      
+      let sebep = args[2]
+      if(!sebep) return message.channel.send(new MessageEmbed().setColor('0x800d0d').setDescription(`Lütfen Bir sebep belirtiniz.`))  
+        
+      let   
+  }
