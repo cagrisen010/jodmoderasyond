1b011c1967b7bd7fb04d8ee46bfc426158ed12e5
Glitch (hello-express) <none>
2020-12-21 06:04:24 +0000

    📒🎬 Checkpoint ./bot.js:22659587/2427 ./komutlar/ramo-afk.js:22659587/4369

--- bot.js
+++ bot.js
@@ -133,32 +133,31 @@ client.randomColor = function () {
 };
 
 
-
-client.on("message" , async ramo => {
+client.on("message" , async msg => {
   
-  if(!ramo.guild) return;
-  if(ramo.content.startsWith(ayarlar.prefix+"afk")) return; 
+  if(!msg.guild) return;
+  if(msg.content.startsWith(ayarlar.prefix+"afk")) return; 
   
-  let afk = ramo.mentions.users.first()
+  let afk = msg.mentions.users.first()
   
-  const kisi = db.fetch(`afkid_${ramo.author.id}_${ramo.guild.id}`)
-
-  const isim = db.fetch(`afkAd_${ramo.author.id}_${ramo.guild.id}`)
+  const kisi = db.fetch(`afkid_${msg.author.id}_${msg.guild.id}`)
+  
+  const isim = db.fetch(`afkAd_${msg.author.id}_${msg.guild.id}`)
  if(afk){
-   const sebep = db.fetch(`afkSebep_${afk.id}_${ramo.guild.id}`)
-   const kisi3 = db.fetch(`afkid_${afk.id}_${ramo.guild.id}`)
-   if(ramo.content.includes(kisi3)){
+   const sebep = db.fetch(`afkSebep_${afk.id}_${msg.guild.id}`)
+   const kisi3 = db.fetch(`afkid_${afk.id}_${msg.guild.id}`)
+   if(msg.content.includes(kisi3)){
 
-       ramo.channel.send(new Discord.MessageEmbed().setColor('BLACK').setDescription(`Etiketlediğiniz Kişi Afk \nSebep : ${sebep}`))
+       msg.channel.send(new Discord.MessageEmbed().setColor('BLACK').setDescription(`<@` + msg.author.id + `> Etiketlediğiniz Kişi Afk \nSebep : ${sebep}`))
    }
  }
-  if(ramo.author.id === kisi){
+  if(msg.author.id === kisi){
 
-       ramo.channel.send(new Discord.MessageEmbed().setColor('BLACK').setDescription(`Afk'lıktan Çıktınız`)
-   db.delete(`afkSebep_${ramo.author.id}_${ramo.guild.id}`)
-   db.delete(`afkid_${ramo.author.id}_${ramo.guild.id}`)
-   db.delete(`afkAd_${ramo.author.id}_${ramo.guild.id}`)
-    ramo.member.setNickname(isim)
+       msg.channel.send(new Discord.MessageEmbed().setColor('BLACK').setDescription(`<@${kisi}> Başarıyla Afk Modundan Çıktınız`))
+   db.delete(`afkSebep_${msg.author.id}_${msg.guild.id}`)
+   db.delete(`afkid_${msg.author.id}_${msg.guild.id}`)
+   db.delete(`afkAd_${msg.author.id}_${msg.guild.id}`)
+    msg.member.setNickname(isim)
     
   }
   
--- komutlar/ramo-afk.js
+++ komutlar/ramo-afk.js
@@ -1,12 +1,12 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require("discord.js");
 const db = require("quick.db");
 exports.run = async (client, message, args) => {
   const kisi = db.fetch(`afkid_${message.author.id}_${message.guild.id}`);
   if (kisi) return;
   const sebep = args[0];
   if (!args[0]) {
-    let member = message.guild.members.cache.get(message.author.id);
-    const strigay = member.displayName;
+    let kullanıcı = message.guild.members.cache.get(message.author.id);
+    const b = kullanıcı.displayName;
 
     await db.set(
       `afkSebep_${message.author.id}_${message.guild.id}`,
@@ -16,33 +16,33 @@ exports.run = async (client, message, args) => {
       `afkid_${message.author.id}_${message.guild.id}`,
       message.author.id
     );
-    await db.set(`afkAd_${message.author.id}_${message.guild.id}`, strigay);
+    await db.set(`afkAd_${message.author.id}_${message.guild.id}`, b);
 
     const ramo = await db.fetch(
       `afkSebep_${message.author.id}_${message.guild.id}`
     );
 
-    message.channel.send(new MessageEmbed().setColor('BLACK').setDescription(`${member} Başarıyla Afk Oldunuz Afk Olmanızın Sebebi: **${ramo}**`));
+   message.channel.send(new MessageEmbed().setColor('BLACK').setDescription(`${kullanıcı} Başarıyla Afk Oldunuz Afk Olmanızın Sebebi: **${ramo}**`));
 
-    message.member.setNickname(`[AFK] ` + strigay);
+    message.member.setNickname(`[AFK] ` + b);
   }
   if (args[0]) {
     let sebep = args.join(" ");
-    let member = message.guild.members.cache.get(message.author.id);
-    const strigay = member.displayName;
+    let kullanıcı = message.guild.members.cache.get(message.author.id);
+    const b = kullanıcı.displayName;
     await db.set(`afkSebep_${message.author.id}_${message.guild.id}`, sebep);
     await db.set(
       `afkid_${message.author.id}_${message.guild.id}`,
       message.author.id
     );
-    await db.set(`afkAd_${message.author.id}_${message.guild.id}`, strigay);
+    await db.set(`afkAd_${message.author.id}_${message.guild.id}`, b);
     const ramo = await db.fetch(
       `afkSebep_${message.author.id}_${message.guild.id}`
     );
 
-    message.channel.send(new MessageEmbed().setColor('BLACK').setDescription(`${member} Başarıyla Afk Oldunuz Afk Olmanızın Sebebi: **${ramo}**`));
+   message.channel.send(new MessageEmbed().setColor('BLACK').setDescription(`${kullanıcı} Başarıyla Afk Oldunuz Afk Olmanızın Sebebi: **${ramo}**`));
 
-    message.member.setNickname(`[AFK] ` + strigay);
+    message.member.setNickname(`[AFK] ` + b);
   }
 };
 
@@ -58,3 +58,4 @@ exports.help = {
   description: "Afk Olmanızı Sağlar.",
   usage: "afk / afk <sebep>"
 };
+
