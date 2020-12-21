62a51971c300d52005d7353dccbfdaa2b878800a
Glitch (hello-express) <none>
2020-12-21 05:39:42 +0000

    🎍🚘 Checkpoint ./komutlar/ramo-mute.js:22659587/2 ./komutlar/ramo-unmute.js:22659587/4694

---
+++ komutlar/ramo-unmute.js
@@ -0,0 +1,69 @@
+const { MessageEmbed } = require('discord.js');
+const data = require('quick.db');
+const ms = require('ms');
+const moment = require('moment')
+
+module.exports.run = async (client, message, args) => {
+if(!["", ""].some(role => message.member.roles.cache.get(role)) && (!message.member.hasPermission("ADMINISTRATOR"))) 
+return message.channel.send(new MessageEmbed().setDescription(`${message.author} Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+ let member = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
+if (!member) return message.channel.send(new MessageEmbed().setColor('0x800d0d').setDescription(`${message.author}, lütfen bir kullanıcı etiketle !`))
+  
+let mute = message.mentions.members.first() || message.guild.members.cache.find(r => r.id === args[0]);
+if (!mute) { new MessageEmbed().setColor('0x800d0d').setDescription(`${message.author}, lütfen mute atmam gereken kullanıcı belirt !`);
+} else {
+   if (mute.roles.highest.position >= message.member.roles.highest.position) 
+      {
+        return message.channel.send(new MessageEmbed().setColor('0x800d0d').setDescription(`Bu Kullanıcı Senden Üst/Aynı Pozisyonda.`));
+      } else {
+      let sebep = args[1]
+      if(!sebep) return message.channel.send(new MessageEmbed().setColor('0x800d0d').setDescription(`Lütfen Bir sebep belirtiniz.`))  
+   
+let tumaylar = {
+"01": "Ocak",  
+"02": "Şubat", 
+"03": "Mart",  
+"04": "Nisan",  
+"05": "Mayıs", 
+"06": "Haziran", 
+"07": "Temmuz",
+"08": "Ağustos", 
+"09": "Eylül", 
+"10": "Ekim", 
+"11": "Kasım", 
+"12": "Aralık", 
+}
+let aylar = tumaylar; 
+       {
+     
+         client.channels.cache.get('763481961611395081').send(
+new MessageEmbed()
+.setAuthor(message.author.username, message.author.avatarURL ({ dynamic: true}))
+.setColor('009caf')
+.setDescription(`<@${member.id}> (\`${member.id}\`) Metin Kanallarına Yazılış Engeli Kaldırıldı.
+
+Yetkili: <@${message.author.id}> (\`${message.author.id}\`)
+
+Sebep: (\`${sebep}\`)
+
+Tarih: (\`${moment(Date.now()).format("DD")} ${aylar[moment(Date.now()).format("MM")]} ${moment(Date.now()).add(10,"hours").format("YYYY HH:mm:ss")}\`)
+
+`))
+mute.roles.remove('763481961565782050')
+message.react('✅')
+} 
+
+
+      }}}
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["unmute"],
+  permLevel: 0,
+  name: "unmute"
+}
+
+exports.help = {
+  name: "unmute"
+};
+
