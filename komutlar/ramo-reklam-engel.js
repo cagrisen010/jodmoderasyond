b322eed02fd33c0c80cee789ee314927c54e6d3f
Glitch (hello-express) <none>
2020-12-22 13:05:45 +0000

    🐚🐆 Checkpoint ./komutlar/ramo-reklam-engel.js:22659587/20156

--- komutlar/ramo-reklam-engel.js
+++ komutlar/ramo-reklam-engel.js
@@ -3,26 +3,28 @@ const db = require('quick.db');
 const ayarlar = require('../ayarlar.json');
 const prefix = ayarlar.prefix
 exports.run = async (client, message, args) => {
-   if(!["", ""].some(role => message.member.roles.cache.get(role)) && (!message.member.hasPermission("ADMINISTRATOR"))) 
-return message.channel.send(new MessageEmbed().setDescription(`${message.author} Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL()({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
-
+     if(!["", ""].some(role => message.member.roles.cache.get(role)) && (!message.member.hasPermission("ADMINISTRATOR"))) 
+return message.channel.send(new MessageEmbed().setDescription(`${message.author} Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+  
 const kanal = message.mentions.channels.first() || message.channel
-const kanall = args[0]
-if(!kanall) return message.channel.send(new MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setTimestamp().setColor('0x800d0d').setDescription(`Sitemi Kullanmak İçin ${prefix}reklam-engel aç Yazmalısın!`))
-if (kanall == 'aç') {
-let açık = await db.fetch(`reklamengelramo_${kanal.id}`)
-if (!açık) return message.channel.send(new MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setTimestamp().setColor('0x800d0d').setDescription(`Reklam Engel Sistemi Zaten Aktif!`))
-
+const ramo = args[0]
+if (!ramo) return message.channel.send(new MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x800d0d').setDescription(`Reklam Engel Sistemini Açmak İçin **${prefix}reklam-engel aç** Yazmalısın`))
+  if (ramo == 'aç') { 
+  let açıkkapalı = await db.fetch(`reklamengelramo_${kanal.id}`)
+  if(açıkkapalı) return message.channel.send(new MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x800d0d').setDescription(`Reklam Engel Sistemi Zaten Aktif!`).then(m => m.delete({timeout: 7000})))
+    
 db.set(`reklamengelramo_${kanal.id}`,'açık')
-message.channel.send(new MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setTimestamp().setColor('0x348f36').setDescription(`Reklam Engel Sistemi Başarıyla Aktif Edildi !`))
-}
-if (kanall == 'kapat') {
-let açık = await db.fetch(`reklamengelramo_${kanal.id}`)
-if (!açık) return message.channel.send(new MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setTimestamp().setColor('0x800d0d').setDescription(`Reklam Engel Sistemi Zaten Aktif Değil!`))
+message.channel.send(new MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x348f36').setDescription(`Reklam Engel Sistemi Başarıyla aktif edildi.`).then(m => m.delete({timeout: 7000})))
+  }
+  
+  if (ramo == 'kapat') {
+  let açıkkapalı = await db.fetch(`reklamengelramo_${kanal.id}`)
+  if(!açıkkapalı) return message.channel.send(new MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x800d0d')(`Reklam Engel Sistemi Zaten Aktif Değil!`).then(m => m.delete({timeout: 7000})))
+    
 db.delete(`reklamengelramo_${kanal.id}`)
-message.channel.send(new MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setTimestamp().setColor('0x348f36').setDescription(`Reklam Engel Sistemi Artık Aktif Değil !`))
+message.channel.send(new MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x800d0d')(`<a:nullsyes:776103575167434802> Reklam engel sistemi başarıyla bu kanalda/belirttiğiniz kanalda deaktif edildi!`).then(m => m.delete({timeout: 7000})))
 }
-}
+};
 exports.conf = {
   enabled: false,
   guildOnly: false,
