da1439367df1350d2023cdfb312d1501d221fc55
Glitch (hello-express) <none>
2020-12-22 18:43:15 +0000

    🛤💝 Checkpoint ./komutlar/striga-küfür-engel.js:26488123/421 ./komutlar/striga-reklam-engel.js:26488123/3236

--- komutlar/striga-reklam-engel.js
+++ komutlar/striga-reklam-engel.js
@@ -1,22 +1,22 @@
 const db = require('quick.db')
 const { Discord, MessageEmbed } = require('discord.js')
- let ayarlar = ['aç','kapat']
+ let ayarlar = require('../ayarlar.json')
+ var prefix = ayarlar.prefix
 exports.run = async (bot, message, args) => {
-  if (!args[0]) return message.channel.send('Hey Bu Ayarı Kullanabilmek için `aç` yada `kapat` yazmalısın.')
-  if(!ayarlar.includes(args[0])) return message.channel.send(`Geçerli parametreleri kullanmalısın.\nParametreler: ${ayarlar.join(' - ')}`)
-  if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('`SUNUCUYU_YÖNET` yetkisine sahip olmalısın!')
+  
+if (!args[0]) return message.channel.send(new MessageEmbed().setDescription(`${message.author} Komutu çalıştırmak için \`aç\` veya \`kapat\` demen gerekiyor.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('`SUNUCUYU_YÖNET` yetkisine sahip olmalısın!')
  
-  if (args[0] == 'aç') {
-    if(db.has(`reklam_${message.guild.id}`)) return message.channel.send(`Sistem zaten açık.`)
-    db.set(`reklam_${message.guild.id}`, 'acik')
-      message.channel.send('Reklam Engel başarıyla açıldı! `Üyeleri Yasakla` yetkisine sahip olanların reklamı engellenmicektir.')
-  }
-  if (args[0] == 'kapat') {
-        if(!db.has(`reklam_${message.guild.id}`)) return message.channel.send(`Sistem zaten kapalı.`)
-    db.delete(`reklam_${message.guild.id}`)
-      message.channel.send('Reklam Engel başarıyla kapatıldı! Artık herkes reklam yapabilir.')
-  }
- 
+if (args[0] == 'aç') {
+if(db.has(`reklam_${message.guild.id}`)) return message.channel.send(new MessageEmbed().setDescription(`${message.author} Komut zaten önceden \`açılmış\`. \n Kapatmak için: \`${prefix}reklam kapat\``).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+db.set(`reklam_${message.guild.id}`, 'acik')
+message.channel.send(new MessageEmbed().setDescription(`${message.author} Reklam filtersini başarıyla \`açtın\`.`).setColor('0x348f36').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+}
+if (args[0] == 'kapat') {
+if(!db.has(`reklam_${message.guild.id}`)) return message.channel.send(new MessageEmbed().setDescription(`${message.author} Komut zaten önceden \`kapatılmış\`. \n Açmak için: \`${prefix}reklam aç\``).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+db.delete(`reklam_${message.guild.id}`)
+message.channel.send(new MessageEmbed().setDescription(`${message.author} Reklam filtersini başarıyla \`kapattın\`.`).setColor('0x348f36').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+}
 };
  
 exports.conf = {
@@ -28,6 +28,6 @@ exports.conf = {
  
 exports.help = {
   name: 'reklam-engelle',
-  description: '[Admin Komutu]',
-  usage: 'reklam-engelle'
+  description: '',
+  usage: ''
 };
