7fd61e237a153a8ddcf1b8ecf000a353ec44e6a4
Glitch (hello-express) <none>
2020-12-23 12:36:48 +0000

    🎌🐋 Checkpoint ./komutlar/striga-snipe.js:26488123/108

--- komutlar/striga-snipe.js
+++ komutlar/striga-snipe.js
@@ -4,19 +4,19 @@ const data = require('quick.db')
    exports.run = async(client, message, args) => {
     const emirhan = await data.fetch(`snipe.id.${message.guild.id}`)
     if(!emirhan) {
-    const codare2 = new MessageEmbed()
+    const embeds = new MessageEmbed()
   .setAuthor(client.user.username, client.user.avatarURL())
   .setDescription(`Mesaj bulunamadı!`)
 .setColor(`#f3c7e1`)
-    message.channel.send(codare2);
+    message.channel.send(embeds);
           } else {
   let kullanıcı = client.users.cache.get(emirhan);
-  const emran = await data.fetch(`snipe.mesaj.${message.guild.id}`)
-  const codare = new MessageEmbed()
+  const silinen = await data.fetch(`snipe.mesaj.${message.guild.id}`)
+  const embed = new MessageEmbed()
   .setAuthor(kullanıcı.username, kullanıcı.avatarURL())
-  .setDescription(`Silinen mesaj: ` + emran)
+  .setDescription(`Silinen mesaj: ` + silinen)
 .setColor(`#f3c7e1`)
-  message.channel.send(codare) }
+  message.channel.send(embed) }
 }
 exports.conf = {
     enabled:true,
