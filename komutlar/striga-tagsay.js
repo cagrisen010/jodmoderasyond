319612c760512e9e632badd8545f19b6ea26d6fc
Glitch (hello-express) <none>
2021-04-24 19:20:13 +0000

    🍄☎️ Checkpoint ./komutlar/striga-tagsay.js:31152063/28 ./komutlar/striga-vmute.js:31152063/2 ./ayarlar.json:31152063/40

--- komutlar/striga-tagsay.js
+++ komutlar/striga-tagsay.js
@@ -3,13 +3,13 @@ module.exports.run = (client, message, args) => {
   
 //-------------------------------------------------------------------------------\\
   
-if(!['ROL ID'].some(role => message.member.roles.cache.get(role)) && !message.member.hasPermission('ADMINISTRATOR')) 
+if(!['834128619910725652'].some(role => message.member.roles.cache.get(role)) && !message.member.hasPermission('ADMINISTRATOR')) 
 return message.channel.send(new MessageEmbed().setDescription(`${message.author} Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
   
 //-------------------------------------------------------------------------------\\  
   
 
-let tag = "tag";
+let tag = "✯";
 const ttag = message.guild.members.cache.filter(m => m.user.username.includes(tag)).size
 
 const embed = new MessageEmbed()
