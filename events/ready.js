41d6eb7622dd85433eaf66e3c95d0e0085fa9173
Glitch (hello-express) <none>
2021-04-25 10:51:44 +0000

    📯🍭 Checkpoint ./events/ready.js:31152063/31

--- events/ready.js
+++ events/ready.js
@@ -7,10 +7,10 @@ var prefix = ayarlar.prefix;
 
 module.exports = client => {
   console.log(`[BOT] | Botun destekçileri ve komutları başarıyla yüklendi, işlemler tamamlandı !`);
-  console.log(`[BOT] | (${client.user.username}) Sepultura!
+  console.log(`[BOT] | (${client.user.username}) Mâqquf?!
   ‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒`);
   client.user.setStatus("online");
   client.user.setActivity("Unsullied ❤️", { type: "PLAYING"}); //// TYPE - WATCHING , PLAYING , LISTENING gibi değiştirilebilir.
-  console.log(`Sepultura Her Daim Sikerr`);
+  console.log(`Mâqquf Her Daim Sikerr`);
 
 };
